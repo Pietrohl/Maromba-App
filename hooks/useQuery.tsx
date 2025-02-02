b08a0b82aad920281cfe0baea57356629bd55c78
Pietro Henrique <pietro_henrique4@hotmail.com>
2025-02-02 14:22:43 -0300

    improvements to useQuery

--- hooks/useQuery.tsx
+++ hooks/useQuery.tsx
@@ -1,22 +1,62 @@
 import { useSystem } from "@/hooks/useSystem";
 import { CompilableQuery } from "@powersync/react-native";
 import { CompiledQuery } from "kysely";
-import { useState, useEffect, useLayoutEffect, useCallback } from "react";
+import { useReducer, useLayoutEffect, useCallback } from "react";
 
 type AdditionalOptions = {
-  queryOnce?: boolean;
+  watch?: boolean;
   executeImmediate?: boolean;
   throttleMs?: number;
 };
 
-const defautOptions: AdditionalOptions = {
-  queryOnce: false,
+const defaultOptions: AdditionalOptions = {
+  watch: true,
   executeImmediate: true,
+  throttleMs: 100,
 };
+
+type State<T> = {
+  isLoading: boolean;
+  isFetching: boolean;
+  data: T[];
+  error?: Error;
+  compiledQuery: CompiledQuery;
+};
+
+type Action<T> =
+  | { type: "SET_FETCHING" }
+  | { type: "SET_DATA"; payload: T[] }
+  | { type: "SET_ERROR"; payload?: Error }
+  | { type: "SET_COMPILED_QUERY"; payload: CompiledQuery };
+
+function reducer<T>(state: State<T>, action: Action<T>): State<T> {
+  switch (action.type) {
+    case "SET_FETCHING":
+      return { ...state, isFetching: true };
+    case "SET_DATA":
+      return {
+        ...state,
+        data: action.payload,
+        isFetching: false,
+        isLoading: false,
+      };
+    case "SET_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+        isFetching: false,
+        isLoading: false,
+      };
+    case "SET_COMPILED_QUERY":
+      return { ...state, compiledQuery: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function useQuery<T = any>(
   query: CompilableQuery<T>,
-  { queryOnce, executeImmediate }: AdditionalOptions = defautOptions
+  params: AdditionalOptions = defaultOptions
 ): {
   isLoading: boolean;
   isFetching: boolean;
@@ -25,44 +65,74 @@ export function useQuery<T = any>(
   error?: Error;
 } {
   const { database } = useSystem();
-  const [isLoading, setIsLoading] = useState(true);
-  const [isFetching, setisFetching] = useState(true);
-  const [compiledQuery, setQuery] = useState(query.compile());
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState<Error>();
+
+  const {
+    executeImmediate = defaultOptions.executeImmediate,
+    watch = defaultOptions.watch,
+    throttleMs = defaultOptions.throttleMs,
+  } = params;
+
+  const [state, dispatch] = useReducer<
+    (state: State<T>, actions: Action<T>) => State<T>
+  >(reducer, {
+    isLoading: true,
+    isFetching: false,
+    data: [],
+    error: undefined,
+    compiledQuery: query.compile() as CompiledQuery<T>,
+  });
 
   const fetchData = useCallback(async () => {
-    setisFetching(true);
-    const results = (
-      await database.executeQuery<T>(compiledQuery as CompiledQuery)
-    ).rows;
-    setisFetching(false);
-    setIsLoading(false);
-    // console.log(`Querying `, queryOnce, "RESULTS ", results);
-    return results;
-  }, [compiledQuery]);
+    dispatch({ type: "SET_FETCHING" });
+    try {
+      const results = (
+        await database.executeQuery<T>(state.compiledQuery as CompiledQuery)
+      ).rows;
+      dispatch({ type: "SET_DATA", payload: results });
+    } catch (error) {
+      dispatch({ type: "SET_ERROR", payload: error as Error });
+    }
+  }, [state.compiledQuery, database]);
 
-  async function refresh() {
-    return fetchData().then(setData).catch(setError);
-  }
+  const watchQuery = useCallback(
+    (abortController: AbortController) => {
+      database.watch(
+        query,
+        {
+          onResult: (results) =>
+            dispatch({ type: "SET_DATA", payload: results }),
+          onError: (error) => dispatch({ type: "SET_ERROR", payload: error }),
+        },
+        { signal: abortController.signal, throttleMs }
+      );
+    },
+    [database, query]
+  );
+
+  const refresh = () => {
+    return fetchData();
+  };
 
   useLayoutEffect(() => {
-    executeImmediate && refresh();
-    !queryOnce &&
-      database.watch<T>(query, {
-        // onResult: (results) => {
-        //   console.log(`Results from Watch : `, results);
-        //   setData(results);
-        // },
-        // onError: (error) => {
-        //   console.log("Error:", error);
-        //   setError(error);
-        // },
-        onResult: setData,
-        onError: setError,
-      });
-    setQuery(query.compile());
-  }, [query, executeImmediate, queryOnce]);
+    const abortController = new AbortController();
 
-  return { isLoading, isFetching, refresh, data, error };
+    if (executeImmediate && !watch) {
+      fetchData();
+    }
+    if (watch) watchQuery(abortController);
+    dispatch({
+      type: "SET_COMPILED_QUERY",
+      payload: query.compile() as CompiledQuery<T>,
+    });
+
+    return () => abortController.abort();
+  }, [query, executeImmediate, watch, database]);
+
+  return {
+    isLoading: state.isLoading,
+    isFetching: state.isFetching,
+    refresh,
+    data: state.data,
+    error: state.error,
+  };
 }
